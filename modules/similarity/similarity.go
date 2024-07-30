16f064571f136e42f2aa8dc56d83675d28a5401f
Arun Subramanian <arunsupe@gmail.com>
2024-07-30 17:27:46 -0500

    corrected key

--- modules/similarity/similarity.go
+++ modules/similarity/similarity.go
@@ -26,7 +26,7 @@ func NewSimilarityCache() *Cache {
 
 func (c *Cache) MemoizedCalculateSimilarity(queryToken, token string, queryVector, tokenVector interface{}) float64 {
 	// similarity is a commutative operation, we can cache the result for any order of operands
-	key := min(queryToken, token) + "|" + max(queryToken, token)
+	key := token
 
 	if cachedValue, exists := c.cache[key]; exists {
 		return cachedValue
